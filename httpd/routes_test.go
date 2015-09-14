7bc6e4287214bed7105ff5ecb31d92c5a507ebc1
David Nix <davidnix81@gmail.com>
2015-09-13 20:10:08 -0600

    add routes test

---
+++ httpd/routes_test.go
@@ -0,0 +1,29 @@
+package httpd
+
+import "testing"
+
+func Test_Routes_routeNames(t *testing.T) {
+	counter := make(map[string]int)
+
+	for _, route := range Routes() {
+		name := route.Name
+
+		counter[name]++
+		if counter[name] > 1 {
+			t.Fatalf("duplicate route name: %q", name)
+		}
+	}
+}
+
+func Test_Routes_routeSignaures(t *testing.T) {
+	counter := make(map[string]int)
+
+	for _, route := range Routes() {
+		signature := route.Path
+
+		counter[signature]++
+		if counter[signature] > 1 {
+			t.Fatalf("duplicate route signature: %q", signature)
+		}
+	}
+}
